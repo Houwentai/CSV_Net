2656749d457030ffcfcd3bc6bc0ccbb7f279c88e
Houwentai <54882741+Houwentai@users.noreply.github.com>
2025-07-11 00:34:37 +0800

    Update prototype_extraction.py

--- prototype_extraction.py
+++ prototype_extraction.py
@@ -54,35 +54,33 @@ def cluster(all_tile_features, n_proto, n_iter, n_init=5, feature_dim=1024, n_pr
 
     return n_patches, weight
 
-# 计算惯性（Inertia）和 Silhouette Score 的 GPU 加速版本（分批处理）
 def compute_inertia(features, centroids, batch_size=10000):
-    # 将数据按批次分割
+    ''' 将数据按批次分割 '''
     n_samples = features.shape[0]
     inertia = 0
 
     for i in range(0, n_samples, batch_size):
         batch = features[i:i+batch_size]
         
-        # 计算每个批次样本到最近簇中心的距离 (Inertia)
-        distances = torch.cdist(batch, centroids)  # 使用GPU上的cdist
-        inertia_batch = torch.min(distances, dim=1)[0].sum()  # 对每个样本找到最小距离并求和
+        distances = torch.cdist(batch, centroids)  ''' 使用GPU上的cdist '''
+        inertia_batch = torch.min(distances, dim=1)[0].sum()  ''' 对每个样本找到最小距离并求和 '''
         inertia += inertia_batch.item()
 
     return inertia
 
 
 if __name__ == "__main__":
-    # 设置CUDA环境
+    ''' 设置CUDA环境 '''
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # 加载患者列表
+    ''' 加载患者列表 '''
     csv_file_path = '../patient_list.csv'
     patient_list = read_first_column_as_list(csv_file_path)
     patient_list = first_column_list[1:]
     print(patient_list)  
   
-    # 加载所有患者的patch特征
+    ''' 加载所有患者的patch特征 '''
     for names in tqdm(len(patient_list)):
         all_tile_features.append(h5py.File(os.path.join(h5dir, names), 'r')["features"][:])
         all_tile_coords.append(h5py.File(os.path.join(h5dir, names), 'r')["coords"][:])
@@ -91,15 +89,15 @@ if __name__ == "__main__":
     all_tile_features = np.vstack(all_tile_features)
     all_tile_coords = np.vstack(all_tile_coords)
     
-    # 存储结果的列表
+    ''' 存储结果的列表 '''
     weights = []
     n_proto = 16
     print("n_proto = {}".format(n_proto))
     
-    # 执行聚类
+    ''' 执行聚类 '''
     _, weight = cluster(all_tile_features, n_proto=n_proto, n_iter=50, n_init=5, feature_dim=1024, n_proto_patches=5000000, mode='faiss', use_cuda=True)
 
-    # 存储权重
+    ''' 存储权重 '''
     weights.append(weight)
     joblib.dump(weights,"../weights_16.pkl")
 
